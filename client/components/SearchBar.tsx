3c205fa0f928a5d076a7596546f22e26c6f3e2a0
Miniapple8888 <miniapple8888@gmail.com>
2021-09-18 15:10:37 -0400

    new client

---
+++ client/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { SafeAreaView, TextInput, View, StyleSheet } from "react-native";
+
+interface SearchBarProps {
+    useInput: [string, React.Dispatch<React.SetStateAction<string>>];
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }, 
+  searchBar: {
+
+  },
+  searchInput: {
+
+  }
+});
+
+export const SearchBar: React.FC<SearchBarProps> = ({ useInput }) => {
+
+    const [input, setInput] = useInput;
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <View style={styles.searchBar}>
+                <TextInput
+                style={styles.searchInput}
+                placeholder="Search"
+                />
+            </View>
+        </SafeAreaView>
+    );
+};
