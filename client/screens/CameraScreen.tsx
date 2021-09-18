609ec25c47a23e7d2a7c247e9c2a8391e05e1b27
Miniapple8888 <miniapple8888@gmail.com>
2021-09-18 19:42:14 -0400

    add expo three

--- client/screens/CameraScreen.tsx
+++ client/screens/CameraScreen.tsx
@@ -1,12 +1,44 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import Expo from 'expo';
+import ExpoTHREE, { three } from 'expo-three';
+import ExpoGraphics from 'expo-graphics';
 
 import { Text, View } from '../components/Themed';
+
+const onContextCreate = async ({gl, scale, width, height, arSession}) => {
+  // Initialize renderer…
+  var renderer = ExpoTHREE.createRenderer({gl});
+  renderer = setPixelRatio(scale);
+  renderer.setSize(width, height);
+  
+  // Initialize scene…
+  var scene = new three.Scene();
+  scene.background =  ExpoTHREE.createARBackgroundTexture(arSession, renderer);
+  
+  // Initialize camera…
+  var camera = ExpoTHREE.createARCamera(arSession, width / scale,
+      height / scale, 0.01, 1000);
+  
+  // Initialize lighting…
+  var ambientLight = new three.AmbientLight(0xaaaaaa);
+  scene.add(ambientLight);
+ }
+
+ const onRender = (renderer, scene, camera) => {
+   renderer.render(scene, camera);
+ }
 
 export default function CameraScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>#TODO: Implement Camera</Text>
+      <ExpoGraphics.View style={{flex:1}}
+         onContextCreate={onContextCreate}
+         onRender={ ()=> {
+           onRender(renderer, scene, camera);
+         } }
+         arEnabled={true}
+       />
     </View>
   );
 }
